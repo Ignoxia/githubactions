3a53d486fb6a885104e72789f433529018283f97
Ignoxia <comptuba@yahoo.com>
2023-04-29 03:11:40 -0400

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,7 +10,6 @@ describe('AppComponent', () => {
         RouterTestingModule,
         HttpClientModule
       ],
-      providers: [FlexSearchService]
       declarations: [
         AppComponent
       ],
