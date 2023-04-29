7a6ad18f7c444f9ab6018ff837479cbc443a37e7
Ignoxia <comptuba@yahoo.com>
2023-04-29 03:13:22 -0400

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,7 +8,7 @@ describe('AppComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        HttpClientModule
+        HttpClientTestingModule 
       ],
       declarations: [
         AppComponent
